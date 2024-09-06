88c959cf7ad2841c74959cbe0c3b05d024e293ee
RichardViso <richardnviso@gmail.com>
2024-09-06 15:46:53 -0500

    js hover

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,24 @@
+const root = document.querySelector(':root');
+const root_styles = getComputedStyle(root);
+const css_green = root_styles.getPropertyValue('--my_green').trim();
+const css_white = root_styles.getPropertyValue('--my_white').trim();
+console.log(css_green);
+
+
 $(document).ready(function() 
 {
+	// link hover functionality
+	$('#nav a').hover(
+		function() 
+		{
+			// on mouse hover change color to my green
+			$(this).css('color', css_green);
 
+		},
+		function()
+		{
+			// go back to original when mouse moves away
+			$(this).css('color', css_white);
+		}
+	);
 });
